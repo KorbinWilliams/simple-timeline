975c9396714d7e00fe8faba9a548616f0be7feb4
KorbinWilliams <korbin.williams@gmail.com>
2022-11-29 13:12:45 -0700

    more setup

---
+++ src/components/mainDisplay/index.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
+import { useSelector } from "react-redux";
+
+const MainDisplay = (props) => {
+  
+
+  return (
+    <div>
+
+    </div>
+  )
+}
---
+++ src/components/mainDisplay/styles.js
@@ -0,0 +1,16 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles({
+  root: {
+    display: 'flex',
+  },
+  example: props => ({
+    zIndex: props.theme.zIndex.drawer + 1,
+    transition: props.theme.transitions.create(['width', 'margin'], {
+      easing: props.theme.transitions.easing.sharp,
+      duration: props.theme.transitions.duration.leavingScreen,
+    }),
+  })
+});
+
+export default styles
