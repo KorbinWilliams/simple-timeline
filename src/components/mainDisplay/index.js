c8f94513fc4e0b9f57ab3d49e4f7a752b7057429
KorbinWilliams <korbin.williams@gmail.com>
2022-11-30 18:28:39 -0700

    chart items look decent, time to separate into different files

--- src/components/mainDisplay/index.js
+++ src/components/mainDisplay/index.js
@@ -3,73 +3,15 @@ import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { useSelector } from "react-redux";
 import { Grid } from '@mui/material'
 import TimelineChart from "../timeline/main";
+import testItems from "./testItems.json"
 
 const MainDisplay = (props) => {
-  const ref = useRef(0)
-
-  let testItems = [
-    {
-      name: 'Bob',
-      // 07:30 - 8:00
-      // appointmentStart: new Date(1669732200000),
-      // appointmentEnd: new Date(1669734000000)
-      appointmentStart: 1669732200000,
-      appointmentEnd: 1669734000000,
-      visitReason: 'check-up'
-    },
-    {
-      name: 'Alena',
-      // 06:15 - 7:45
-      // appointmentStart: new Date(1669727700000),
-      // appointmentEnd: new Date(1669733100000)
-      appointmentStart: 1669727700000,
-      appointmentEnd: 1669733100000,
-      visitReason: 'colonoscopy'
-    },
-    {
-      name: 'Joseph',
-      // 15:45 - 16:45
-      // appointmentStart: new Date(1669761900000),
-      // appointmentEnd: new Date(1669765500000)
-      appointmentStart: 1669761900000,
-      appointmentEnd: 1669765500000,
-      visitReason: 'diabetes consultation'
-    },
-    {
-      name: 'Stephanie',
-      // 12:00 - 12:42
-      // appointmentStart: new Date(1669748400000),
-      // appointmentEnd: new Date(1669750920000)
-      appointmentStart: 1669748400000,
-      appointmentEnd: 1669750920000,
-      visitReason: 'physical exam'
-    },
-    {
-      name: 'Ricardo',
-      // 14:30 - 15:35
-      // appointmentStart: new Date(1669757400000),
-      // appointmentEnd: new Date(1669761300000)
-      appointmentStart: 1669757400000,
-      appointmentEnd: 1669761300000,
-      visitReason: 'ultrasound & MRI'
-    },
-    {
-      name: 'Jhon',
-      // 10:15 - 11:00
-      // appointmentStart: new Date(1669742100000),
-      // appointmentEnd: new Date(1669744800000)
-      appointmentStart: 1669742100000,
-      appointmentEnd: 1669744800000,
-      visitReason: 'adhd follow-up'
-    }
-  ]
+  // const ref = useRef(0)
 
   const formatAppointments = (aptsArr) => {
-    console.log(`aptsArr:`, aptsArr)
     for (let i = 0; i < aptsArr.length; i++) {
       let apt = aptsArr[i];
       // console.log('start: ', new Date(apt.appointmentStart).toISOString(), ' end: ', new Date(apt.appointmentEnd).toISOString())
-      console.log(apt.name, apt.appointmentEnd - apt.appointmentStart)
       apt.label = [
         {
           text: [`${apt.visitReason}`],
@@ -84,17 +26,36 @@ const MainDisplay = (props) => {
     return aptsArr
   }
 
+  const getChartRange = (aptsArr, type) => {
+    let time
+
+    for (let i = 0; i < aptsArr.length; i++) {
+      const apt = aptsArr[i];
+      if (type === 'start') {
+        let aptStart = new Date(apt.appointmentStart).getHours()
+        time = !time || aptStart < time ? aptStart : time
+      }
+      else {
+        let aptEnd = new Date(apt.appointmentEnd).getHours()
+        time = !time || aptEnd > time ? aptEnd : time
+      }
+    }
+
+    return time
+  }
+
   // TODO // simplify starting chart (add more default options, enable more types for css options)
   // TODO // make it so label has a default using text from label property
+  // TODO // fix chartWidth to use pixels or % instead of a number
 
   return (
-    <Grid container item xs={12} ref={ref}>
+    <Grid container item xs={12}>
       <TimelineChart
         items={formatAppointments(testItems)}
         // Nov 29 2022 00:00:00
         timelineDate={1669705200000}
-        timelineStart={5}
-        timelineEnd={23}
+        timelineStart={getChartRange(testItems, 'start')}
+        timelineEnd={getChartRange(testItems, 'end')}
         showCurrentTime={true}
         dateItemsLoc={['appointmentStart', 'appointmentEnd']}
         vAxisProp={'name'}
